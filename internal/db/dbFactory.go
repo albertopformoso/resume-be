90d38d98c562440994e63341cd41afe4f86652d6
albertopformoso <albertopformoso@gmail.com>
2022-07-07 11:49:13 -0500

    db driver pg implemented

--- internal/db/dbFactory.go
+++ internal/db/dbFactory.go
@@ -1,1 +1,30 @@
 package db
+
+import (
+	"log"
+
+	"github.com/albertopformoso/resume-be/internal/model"
+)
+
+// Database supported driver list
+const (
+	DriverUndefined model.DBDriver = 0
+	DriverPG        model.DBDriver = 1
+)
+
+// DB database interface
+type DB interface {
+}
+
+func NewDB(driver model.DBDriver) DB {
+	switch driver {
+	case DriverPG:
+		return &postgres{}
+	case DriverUndefined:
+		log.Printf("ERROR: driver %q not supported", driver)
+		return nil
+	default:
+		log.Printf("ERROR: driver %q not supported", driver)
+		return nil
+	}
+}
--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,1 +1,46 @@
 package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	once            sync.Once
+	postgresConnStr string = os.Getenv("DATABASE_URL")
+)
+
+// postgres GORM postgresql driver implementation
+type postgres struct {
+	conn *gorm.DB
+}
+
+// newPostgresDB stablishes a new postgres connection
+func (p *postgres) newPostgresDB() {
+	once.Do(func() {
+		var err error
+		p.conn, err = gorm.Open("postgres", postgresConnStr)
+		if err != nil {
+			log.Fatalf("ERROR: can't open db: %v", err)
+		}
+
+		fmt.Println("Connected to PostgreSQL")
+	})
+}
+
+// Pool get the connection to the postgres db
+func (p *postgres) Pool() *gorm.DB {
+	p.newPostgresDB()
+	return p.conn
+}
+
+// closeDBConnection closes the conection to postgresql db
+func (p *postgres) closeDBConnection() {
+	p.conn.Close()
+	log.Println("Connection to PostgreSQL closed")
+}
